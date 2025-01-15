1afb779b01768cb04caf7dd665ec2fd291a20ab2
Bentroy <bentroy006@gmail.com>
2025-01-15 15:56:10 +0100

    yhh

---
+++ js/new.js
@@ -0,0 +1,21 @@
+"use strict";
+
+//ARRAY
+//LOOP
+
+
+let fruit = [];
+
+let cars ={
+    name: "Toyota",
+    city: "Lagos",
+    country: "Nigeria"
+}
+
+
+
+let fruits = ["Apple", "Orange", "Mango"];
+fruits.push("Mango");
+console.log(fruits);
+
+
