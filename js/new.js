f9ca5085179700137b1d418af306ed2627f4048f
Bentroy <bentroy006@gmail.com>
2025-01-15 16:51:00 +0100

    playing around, learnt if/else, learnt switch, while and for satement

--- js/new.js
+++ js/new.js
@@ -1,30 +1,67 @@
 "use strict";
 
-//ARRAY
-//LOOP
+// //ARRAY
+// //LOOP
 
 
-let fruit = [];
+// // let fruit = [];
+// // let fruits = ["Apple", "Orange", "Mango"];
+// // fruits.push("kiwi");
+// // console.log(fruits);
 
-let cars ={
-    name: "Toyota",
-    city: "Lagos",
-    country: "Nigeria"
+// // fruits.pop()
+// // console.log(fruits);
+
+// // fruits.shift()
+// // console.log(fruits);
+
+// // fruits.splice(0,1, 'kiwi', 'egg', 'apple')
+// // console.log(fruits);
+
+
+// // for (let i = 0; i < 5; i++) {
+// //     console.log(i)
+    
+// // }
+
+// // let noFruit = 1;
+// // while (noFruit < 5) {
+// //     console.log(noFruit);
+// //     noFruit++;
+// // }
+
+
+
+// switch (dayOfTheWeek) {
+//     case "Monday":
+//         console.log("Today is Monday");
+//         break;
+
+//     case "Tuesday":
+//         console.log("Today is Tuesday");
+//         break;
+
+//     case "Wednesday":
+//         console.log("Today is Wednesday");
+//         break;
+
+//     case "Thursday":
+//         console.log("Today is Thursday");
+//         break;
+
+//     case "Friday":
+//         console.log("Today is Friday");
+//         break;
+
+//     default :
+//         console.log("Today is Weekend");
+//         break;
+// }
+
+
+
+let ageOf = 18;
+
+if(ageOf > 17) {
+    console.log("You are an adult");
 }
-
-
-
-let fruits = ["Apple", "Orange", "Mango"];
-fruits.push("kiwi");
-console.log(fruits);
-
-
-
-fruits.pop()
-console.log(fruits);
-
-fruits.shift()
-console.log(fruits);
-
-fruits.splice(0,1, 'kiwi', 'egg', 'apple')
-console.log(fruits);
